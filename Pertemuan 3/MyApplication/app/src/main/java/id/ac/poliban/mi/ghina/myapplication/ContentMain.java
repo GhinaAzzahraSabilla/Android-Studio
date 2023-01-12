59c5590c983f3ba77142ddda19525f92833c6d86
GhinaAzzahraSabilla <ghinasabilla11@gmail.com>
2023-01-12 15:26:53 +0800

    UTS

---
+++ Pertemuan 3/MyApplication/app/src/main/java/id/ac/poliban/mi/ghina/myapplication/ContentMain.java
@@ -0,0 +1,15 @@
+package id.ac.poliban.mi.ghina.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContentMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_main);
+
+    }
+}
